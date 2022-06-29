2bcdb75ffa4abb1f8a785bed70bf7ee867ea7002
kinya ruth <45145197+kinyaruthm@users.noreply.github.com>
2022-06-29 21:14:27 +0300

    Add files via upload

---
+++ Matrix.java
@@ -0,0 +1,25 @@
+
+package hackerranksolution;
+
+
+public class Matrix extends ArrayIndexOutOfBoundsException{
+    public static void main(String[]args)
+    {
+      
+        int x[][]={
+            {2,3,4},
+            {5,6,7},
+            {5,6,7}
+                    };
+               
+        for(int i=0;i<=x.length;i++)
+        {
+            for(int j=0;j<x[i].length;j++)
+            {
+      
+            System.out.print(x[i][j] + " ");
+        }{
+         System.out.println();
+    }}}}
+    
+
